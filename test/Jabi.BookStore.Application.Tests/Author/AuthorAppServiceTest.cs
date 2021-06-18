54676ca776157a6d37d2b004ae1e90dd9ae6cba0
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-06-18 15:23:38 +0400

    Added Author testing IAuthorService

---
+++ test/Jabi.BookStore.Application.Tests/Author/AuthorAppServiceTest.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jabi.BookStore.Services.Authors;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using NSubstitute;
+using NSubstitute.Extensions;
+using Shouldly;
+using Volo.Abp.Application.Dtos;
+using Xunit;
+
+namespace Jabi.BookStore.Author
+{
+    public sealed class AuthorAppServiceTest : BookStoreApplicationTestBase
+    {
+        private readonly IAuthorAppService _authorAppService;
+        public AuthorAppServiceTest()
+        {
+            _authorAppService = GetRequiredService<IAuthorAppService>();
+        }
+
+        [Fact]
+        public async Task CreateAsync_InsertAuthor_ReturnCreateAuthorDto()
+        {
+            var service = _authorAppService.CreateAsync(new CreateAuthorDto()
+            {
+                BirthDate = new DateTime(2323,3,23),
+                Name = "Jabi",
+                ShortBio = "Salam"
+            });
+             await service.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task GetListAsync_IsNullOrWhiteSpace_ReturnAuthorName()
+        {
+            //Act
+            var result = await _authorAppService.GetListAsync(
+                new GetAuthorListDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(b => b.Name == "Test Author1");
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public async Task UpdateAsync_UpdateAuthor_ReturnUpdateAuthorDto(int authorId)
+        {
+            var service = _authorAppService.UpdateAsync(authorId, new UpdateAuthorDto()
+            {
+                BirthDate = new DateTime(2323, 3, 23),
+                ShortBio = "Salam"
+            });
+            await service.ShouldNotBeNull();
+            //service.ShouldBeOfType<UpdateAuthorDto>();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public async Task DeleteAsync_DeleteAuthor_ReturnDeletedAuthor(int authorId)
+        {
+            var service = _authorAppService.DeleteAsync(authorId);
+          await  service.ShouldNotBeNull();
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public async Task GetAsync_GetAuthorWithId_ReturnGetAuthor(int authorId)
+        {
+            var service = _authorAppService.GetAsync(authorId);
+            await service.ShouldNotBeNull();
+        }
+
+    }
+}
--- test/Jabi.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
+++ test/Jabi.BookStore.TestBase/BookStoreTestDataSeedContributor.cs
@@ -1,14 +1,78 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Jabi.BookStore.Books;
+using Jabi.BookStore.Entity.Authors;
+using Jabi.BookStore.Entity.Books;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
 
 namespace Jabi.BookStore
 {
     public class BookStoreTestDataSeedContributor : IDataSeedContributor, ITransientDependency
     {
+        private readonly IRepository<Book, int> _bookRepository;
+        private readonly IRepository<Author, int> _authorRepository;
+
+        public BookStoreTestDataSeedContributor(IRepository<Book, int> bookRepository, IRepository<Author, int> authorRepository)
+        {
+            _bookRepository = bookRepository;
+            _authorRepository = authorRepository;
+        }
+
         public Task SeedAsync(DataSeedContext context)
         {
-            /* Seed additional test data... */
+            _authorRepository.InsertAsync(
+                new Author(name: "Test Author1",
+                    birthDate: new DateTime(2021, 2, 3),
+                    shortBio: "Test Bio1")
+                );
+            _authorRepository.InsertAsync(
+                new Author(name: "Test Author2",
+                    birthDate: new DateTime(2022, 2, 3),
+                    shortBio: "Test Bio2")
+                );
+            _authorRepository.InsertAsync(
+                new Author(name: "Test Author3",
+                    birthDate: new DateTime(2023, 2, 3),
+                    shortBio: "Test Bio3")
+                );
+
+
+            /*Books*/
+            _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Test Book1",
+                    Type = BookType.Adventure,
+                    PublishDate = new DateTime(2021,1,1),
+                    Description = "Test Description1",
+                    IsDeleted = false,
+                    AuthorId = 1
+                }
+            );  
+            _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Test Book2",
+                    Type = BookType.Adventure,
+                    PublishDate = new DateTime(2022,2,2),
+                    Description = "Test Description2",
+                    IsDeleted = false,
+                    AuthorId = 2
+                }
+            ); 
+            _bookRepository.InsertAsync(
+                new Book
+                {
+                    Name = "Test Book3",
+                    Type = BookType.Adventure,
+                    PublishDate = new DateTime(2023,3,3),
+                    Description = "Test Description3",
+                    IsDeleted = false,
+                    AuthorId = 3
+                }
+            );
 
             return Task.CompletedTask;
         }
